99a554eb10ece06025292205913d3f697d3f335c
Renan Fonseca Afrausino <rafrausin@Admins-MBP.home>
2020-08-08 10:31:13 -0300

    :tada:(+)Project has done code class 4 discovery

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const proffys = [
+  {
+    nome: "Diego Fernandes",
+    avatar:
+      "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
+    whatsapp: "8998765434",
+    bio:
+      "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+  },
+  {
+    nome: "Diego Fernandes",
+    avatar:
+      "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
+    whatsapp: "8998765434",
+    bio:
+      "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220],
+  },
+];
+
+function pageLanding(req, res) {
+  return res.sendFile(__dirname + "/views/index.html");
+}
+
+function pageStudy(req, res) {
+  return res.sendFile(__dirname + "/views/study.html");
+}
+
+function pageGiveClasses(req, res) {
+  return res.sendFile(__dirname + "/views/give-classes.html");
+}
+
+const express = require("express");
+const server = express();
+
+server
+  .use(express.static("public"))
+  .get("/", pageLanding)
+  .get("/study", pageStudy)
+  .get("/give_classes", pageGiveClasses)
+
+  .listen(5500);
