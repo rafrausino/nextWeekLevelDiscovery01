813a62199816bd86abcd6553cc49c86378dad2dd
Renan Fonseca Afrausino <rafrausin@Admins-MacBook-Pro.local>
2020-08-17 10:55:37 -0300

    :tada:(+)Project has done code class 5 discovery

--- webNunjucks/public/scripts/addFields.js
+++ webNunjucks/public/scripts/addFields.js
@@ -1,25 +1,15 @@
-// Procurar o botao
-document
-  .querySelector("#add-time")
-  // Quano clicar no botao
-  .addEventListener("click", cloneField);
+document.querySelector("#add-time").addEventListener("click", cloneField);
 
-// Executar uma acao
 function cloneField() {
-  // Duplicar os campos. Que campos?
-  const newFielContainer = document
-    .querySelector(".schedule-items")
+  const newFieldContainer = document
+    .querySelector(".schedule-item")
     .cloneNode(true);
 
-  // pegar cada campo. que campos?
-  const fields = newFielContainer.querySelectorAll("input");
+  const fields = newFieldContainer.querySelectorAll("input");
 
-  // para cada campo, limpar
   fields.forEach(function (field) {
-    // pega o field do momento e limpa ele
     field.value = "";
   });
 
-  // Colocar na página: onde??
-  document.querySelector("#schedule-item").appendChild(newFielContainer);
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
 }
--- webNunjucks/src/server.js
+++ webNunjucks/src/server.js
@@ -1,104 +1,13 @@
-const proffys = [
-  {
-    name: "Diego Fernandes",
-    avatar:
-      "https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
-    whatsapp: "8998765434",
-    bio:
-      "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
-    cost: "20",
-    weekday: [0],
-    time_from: [720],
-    time_to: [1220],
-  },
-  {
-    name: "Renan Afrausino",
-    avatar:
-      "https://avatars2.githubusercontent.com/u/3664254?s=460&u=73c9a8ab65243b0e10d891ac69043a174f8e2c84&v=4",
-    whatsapp: "51997200198",
-    bio:
-      "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Fullstack",
-    cost: "80",
-    weekday: [0],
-    time_from: [720],
-    time_to: [1220],
-  },
-  {
-    name: "Maky Brito",
-    avatar:
-      "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-    whatsapp: "4899123654",
-    bio:
-      "An instructor focused on helping people start programming for web - #html #css #javascript #sql #react #nodejs #fullstack",
-    subject: "Algoritmo",
-    cost: "40",
-    weekday: [0],
-    time_from: [720],
-    time_to: [1220],
-  },
-];
-
-const subjects = [
-  "Artes",
-  "Biologo",
-  "Ciência",
-  "Educação Fisica",
-  "Física",
-  "Geografia",
-  "História",
-  "Matématica",
-  "Português",
-  "Química",
-  "Algoritmo",
-];
-
-const weekdays = [
-  "Segunda-feira",
-  "Terça-feira",
-  "Quarta-feira",
-  "Quinta-feira",
-  "Sexta-feira",
-  "Sábado",
-];
-
-//Funcionalidades
-
-function getSubject(subjectNumber) {
-  const position = +subjectNumber - 1;
-  return subjects[position];
-}
-function pageLanding(req, res) {
-  return res.render("index.html");
-}
-
-function pageStudy(req, res) {
-  const filters = req.query;
-  return res.render("study.html", { proffys, filters, subjects, weekdays });
-}
-
-function pageGiveClasses(req, res) {
-  const data = req.query;
-
-  //Se tiver dados(data)
-  const isNotEmpty = Object.keys(data).length > 0;
-  if (isNotEmpty) {
-    data.subject = getSubject(data.subject);
-
-    //Adicionar data ao a lista de proffys
-    proffys.push(data);
-
-    return res.redirect("/study");
-  }
-
-  //Se não, não mostrar a página
-  return res.render("give-classes.html", { subjects, weekdays });
-}
-
 //Servidor
 const express = require("express");
 const server = express();
+
+const {
+  pageLanding,
+  pageStudy,
+  pageGiveClasses,
+  saveClasses,
+} = require("./pages");
 
 //configura nunjucks (template engine)
 const nunjucks = require("nunjucks");
@@ -109,11 +18,14 @@ nunjucks.configure("src/views", {
 
 //Início e configuração do servidor
 server
+  // receber os dados do req.body
+  .use(express.urlencoded({ extended: true }))
   //configurar arquivos estáticos (css, scripts, images)
   .use(express.static("public"))
   //rotas da aplicação
   .get("/", pageLanding)
   .get("/study", pageStudy)
   .get("/give-classes", pageGiveClasses)
+  .post("/save-classes", saveClasses)
   // start do servidor
   .listen(5500);
